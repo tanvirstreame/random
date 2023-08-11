2935530368c0c3526b6aba591a14f02a5ff8e0a9
Tanvir Islam Streame <engr.tanvir.islam.1@gmail.com>
2023-08-12 00:23:15 +0600

    First commit

---
+++ localStorage/localStorage.js
@@ -0,0 +1,21 @@
+class LocalStorage {
+  addItem(data, key) {
+    // Set local storage with key
+
+  }
+
+  getItem(key) {
+    // Get local storage with key
+
+  }
+
+  removeItem(key) {
+    // Remove local storage with key
+
+  }
+
+  removeAllItem() {
+    // Clear all from local storage
+
+  }
+}
