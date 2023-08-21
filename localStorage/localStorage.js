f58349f82f638106b3094ee886590604d897144f
asifgenius <asif15-11288@diu.edu.bd>
2023-08-21 23:17:30 +0600

    Added setItem, getItem and removeItem from localStroage

--- localStorage/localStorage.js
+++ localStorage/localStorage.js
@@ -1,21 +1,22 @@
 class LocalStorage {
   addItem(data, key) {
     // Set local storage with key
-
+    return localStorage.setItem(key, JSON.stringify(data));
   }
 
   getItem(key) {
     // Get local storage with key
-
+    var items = JSON.parse(localStorage.getItem(key) || "[]");
+    return items;
   }
 
   removeItem(key) {
     // Remove local storage with key
-
+    return localStorage.removeItem(key);
   }
 
   removeAllItem() {
     // Clear all from local storage
-
+    return localStorage.clear();
   }
 }
--- localStorage/scripts.js
+++ localStorage/scripts.js
@@ -1,3 +1,47 @@
-const addData = () => {
+const itemList = new LocalStorage();
+document.getElementById("text-submit-massage").innerHTML = `Give Your Name, Email, Age`
 
+const addData = () => {
+  const itemList = new LocalStorage();
+  var arrayData = itemList.getItem("item") || [];
+  arrayData.push({
+    name: document.getElementById("full_name").value,
+    age: document.getElementById("age").value,
+    email: document.getElementById("email").value
+  })
+  document.getElementById("text-submit-massage").innerHTML = `Successfully Added Name: ${arrayData[arrayData.length - 1].name}`
+  itemList.addItem(arrayData, "item");
+  populateTable();
+}
+
+function populateTable() {
+  const itemList = new LocalStorage();
+  var items = itemList.getItem("item");
+  document.getElementById("text").innerHTML =
+    `<table>
+      <tr>
+      <th>Full Name</th>
+      <th>Email</th>
+      <th>Age</th>
+      </tr>
+    </table>`
+  items.map(item =>
+    document.getElementById("text").innerHTML +=
+    `<table>
+      <tr>
+        <td>${item.name}</td>
+        <td>${item.email}</td>
+        <td>${item.age}</td>
+      </tr>
+    </table >`
+  )
+}
+populateTable();
+
+const removeItem = () => {
+  return itemList.removeItem('item')
+}
+
+const removeAllItem = () => {
+  return itemList.removeAllItem()
 }
